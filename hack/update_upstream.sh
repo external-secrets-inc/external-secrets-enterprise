14739cff737356ced9642c4801c579c1e88506fa
Gustavo Carvalho <gustavo@externalsecrets.com>
2025-05-09 10:52:44 -0300

    chore: update upstream
    Signed-off-by: Gustavo Carvalho <gustavo@externalsecrets.com>

---
+++ hack/update_upstream.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+## No need to do this as we are already checked-out via actions
+# git clone git@github.com:external-secrets-inc/external-secrets-enterprise
+# cd external-secrets-enterprise
+
+git remote add upstream git@github.com:external-secrets/external-secrets.git
+git remote set-url upstream --push DISABLE
+
+git fetch upstream
+
+git checkout -b update-upstream-$(date +%s)
+
+git merge upstream/main --no-ff --no-commit || true
+
+#verify if any files were added. If they were, commit them. If not, skip safely
+if [[ -z "$(git status --porcelain)" ]]; then
+  echo "nothing changed. skipping."
+  exit 0;
+fi
+
+git add .
+git commit -s -m "chore: update upstream"
+
+git push origin update-upstream-$(date +%s)
+
+gh pr create --title "chore: update upstream" --body "update upstream" --base main --head update-upstream-$(date +%s)
