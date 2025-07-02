8a28925b8d0dd5e6a05275f552d131b5d3097b8b
Gustavo Carvalho <gustavo@externalsecrets.com>
2025-07-02 11:15:44 -0300

    fix: lint
    Signed-off-by: Gustavo Carvalho <gustavo@externalsecrets.com>

--- pkg/template/v2/esi_custom.go
+++ pkg/template/v2/esi_custom.go
@@ -4,7 +4,7 @@ package template
 
 import "encoding/json"
 
-// Verifies if given string is a valid json object
+// Verifies if given string is a valid json object.
 func isJson(str string) bool {
 	var js map[string]interface{}
 	return json.Unmarshal([]byte(str), &js) == nil
