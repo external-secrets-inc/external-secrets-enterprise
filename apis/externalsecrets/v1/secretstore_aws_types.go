d2b174524a3c6f6da46810cbb09ee4d947efcd3a
Gustavo Carvalho <gustavo@externalsecrets.com>
2025-08-11 11:56:46 -0300

    fix: tests
    Signed-off-by: Gustavo Carvalho <gustavo@externalsecrets.com>

--- apis/externalsecrets/v1/secretstore_aws_types.go
+++ apis/externalsecrets/v1/secretstore_aws_types.go
@@ -99,7 +99,7 @@ type AWSProvider struct {
 	// if not set aws sdk will infer credentials from your environment
 	// see: https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#specifying-credentials
 	// +optional
-	Auth AWSAuth `json:"auth,omitempty"`
+	Auth *AWSAuth `json:"auth,omitempty"`
 
 	// Role is a Role ARN which the provider will assume
 	// +optional
