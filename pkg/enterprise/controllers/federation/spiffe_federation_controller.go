df64b9302b8c48a79d7ae7e49618d3a36c24aec9
msfernandes <matheus.souza.fernandes@gmail.com>
2025-10-02 22:30:43 -0300

    fix: adjust import for the new identity module

--- pkg/enterprise/controllers/federation/spiffe_federation_controller.go
+++ pkg/enterprise/controllers/federation/spiffe_federation_controller.go
@@ -22,7 +22,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 
-	"github.com/external-secrets/external-secrets/apis/enterprise/federation/v1alpha1"
+	idfedv1alpha1 "github.com/external-secrets/external-secrets/apis/enterprise/federation/identity/v1alpha1"
+	fedv1alpha1 "github.com/external-secrets/external-secrets/apis/enterprise/federation/v1alpha1"
 	"github.com/external-secrets/external-secrets/pkg/enterprise/federation/provider"
 	"github.com/external-secrets/external-secrets/pkg/enterprise/federation/store"
 )
@@ -36,11 +37,11 @@ type SpiffeFederationController struct {
 
 func (c *SpiffeFederationController) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
 	// Get the Authorization.fedetarion.external-secrets.io object
-	authorization := &v1alpha1.SpiffeFederation{}
+	authorization := &idfedv1alpha1.SpiffeFederation{}
 	if err := c.Get(ctx, req.NamespacedName, authorization); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-	ref := v1alpha1.FederationRef{
+	ref := fedv1alpha1.FederationRef{
 		Name: authorization.Name,
 		Kind: "SpiffeFederation",
 	}
@@ -53,6 +54,6 @@ func (c *SpiffeFederationController) Reconcile(ctx context.Context, req ctrl.Req
 // SetupWithManager returns a new controller builder that will be started by the provided Manager.
 func (c *SpiffeFederationController) SetupWithManager(mgr ctrl.Manager, opts controller.Options) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&v1alpha1.SpiffeFederation{}).
+		For(&idfedv1alpha1.SpiffeFederation{}).
 		Complete(c)
 }
